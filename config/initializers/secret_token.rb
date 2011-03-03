f85a3aecb92f90cfcde07ea78d73c64685b195eb
Rom Srinivasan <roms@benetech.org>
2011-03-03 13:52:27 -0800

    initial commit of default rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiagramRailsApp::Application.config.secret_token = 'cd486b2b1f5f278334a0cc13eee3d81f09ffd8935eb339e9b8a3ef75cd68706c2aa2994d923632ee5295e11847ed8a20b1aea1c1a390296aa1e9b03176ee503e'
