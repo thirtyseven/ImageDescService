83961a138b58a4e618a6b5a86a22dcb8ebbed730
kevins <kevins@benetech.org>
2011-04-22 17:32:21 -0400

    IMG-38: Not done yet, but parsing the book xml and finding matching images in the database; returning summary of found images instead of the book, for now

--- app/controllers/update_descriptions_in_book_controller.rb
+++ app/controllers/update_descriptions_in_book_controller.rb
@@ -1,6 +1,31 @@
+require "rexml/document"
+
 class UpdateDescriptionsInBookController < ApplicationController
   def upload
+    result = []
+    
     book = params[:book]
-    render :text => book.read
+    file = File.new( book.path )
+    doc = REXML::Document.new file
+    
+    xpath_uid = "//meta[@name='dtb:uid']/@content"
+    book_uid = REXML::XPath.first(doc, xpath_uid).to_s
+    result << "Book uid: #{book_uid}<br/>"
+
+    xpath_title = "//meta[@name='dc:Title']/@content"
+    book_title = REXML::XPath.first(doc, xpath_title).to_s
+    result << "Book Title: #{book_title}<br/>"
+
+    matching_images = DynamicImage.where("uid = ?", book_uid)
+    matching_images.each do | dynamic_image |
+      image_location = dynamic_image.image_location
+      element = REXML::XPath.first( doc, "//img[@src='#{image_location}']")
+      result << "#{element.attributes['id']} #{image_location}<br/>"
+      dynamic_image.dynamic_descriptions.each do | description |
+        result << "&nbsp;&nbsp;#{description.updated_at} #{description.body} <br/>"
+      end
+    end
+
+    render :text => result.join("\n")
   end
 end
