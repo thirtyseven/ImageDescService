fe8135c864932a611123d598f71332515384d9bb
Rom Srinivasan <roms@benetech.org>
2011-09-06 12:04:15 -0700

    reducing thread pool size to 8

--- app/controllers/daisy_book_controller.rb
+++ app/controllers/daisy_book_controller.rb
@@ -278,7 +278,7 @@ class DaisyBookController < ApplicationController
     end
 
     #upload the files, if they have not been previously uploaded, to s3 in parallel
-    Parallel.map(files.keys, :in_threads => 16) do |file_key|
+    Parallel.map(files.keys, :in_threads => 8) do |file_key|
 
       # upload files
         s3_object = bucket.objects[file_key]
