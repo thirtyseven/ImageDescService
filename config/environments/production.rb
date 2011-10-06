90419755ebb1de783272dcae4813d5b8cff0aa08
Rom Srinivasan <roms@benetech.org>
2011-10-06 10:51:13 -0700

    added environment variable for host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ DiagramRailsApp::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'herokuapp.com' }
+  config.action_mailer.default_url_options = ENV['POET_HOST']
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
