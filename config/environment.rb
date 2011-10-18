771673a61ff19379bca7f0a92709ba48334b75fd
Rom Srinivasan <roms@benetech.org>
2011-10-18 13:27:49 -0700

    date added now included in books page

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 DiagramRailsApp::Application.initialize!
+
+Time::DATE_FORMATS[:poet_default] = "%b %d, %Y"
