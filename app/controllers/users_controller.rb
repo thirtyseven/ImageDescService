f449fa5e5112a243fc2adb57c02ae6cc30cf6d41
Rom Srinivasan <roms@benetech.org>
2011-10-26 16:24:13 -0700

    added ordering by date created for users and required login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!
+
   def index
-    @users = User.all
+    #@users = User.all
+    @users = User.find(:all, :order => "created_at")
   end
 end
