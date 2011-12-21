5ee8cb14217ba767f9d99df66f194837ce50bac4
Eric Hansen <ehansen486@gmail.com>
2011-12-20 21:26:18 -0800

    IMG-222 Add an admin interface for books fix the javascript messaging

--- public/javascripts/active_admin_books.js
+++ public/javascripts/active_admin_books.js
@@ -1,12 +1,18 @@
 jQuery(function($) {
-  // create a convenient toggleLoading function
-  var toggleLoading = function() { $("#loading").toggle() };
+	// create a convenient toggleLoading function
+	var toggleLoading = function() { $("#loading").toggle() };
 
   $(".book-link-ajax")
-     .bind("ajax:success", function(event, data, status, xhr) {
-       alert( "success!");
-     }) 
-     .bind("ajax:failure", function(event, data, status, xhr) {
-       alert( "failure!");
-     });
+  	.bind("ajax:success", function(event, data, status, xhr) {
+			var actionDiv = $(this).parents('.action');
+			actionDiv.text("success");
+			actionDiv.css('fontWeight', "bold");
+	    actionDiv.css('color', "orange");
+    }) 
+  .bind("ajax:failure", function(event, data, status, xhr) {
+			var actionDiv = $(this).parents('.action');
+			actionDiv.text("failure");
+			actionDiv.css('fontWeight', "bold");
+	    actionDiv.css('color', "red");
+    });
 });
