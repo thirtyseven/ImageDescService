5d98e700a10414051420f0995a7eb0af228046ec
Eric Hansen <ehansen486@gmail.com>
2012-01-04 12:00:05 -0800

    fix environment name

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,4 @@
 Airbrake.configure do |config|
   config.api_key = 'c4c05698069fc1c45c6289d79339ad0c'
-  config.environment_name = "<%=ENV['RACK_ENV']%>"
+  config.environment_name = ENV['RACK_ENV']
 end
