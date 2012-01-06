fa75894b916cc5cb4bd1728c0361a6d467eef46a
Rom Srinivasan <roms@benetech.org>
2012-01-05 16:05:20 -0800

    debugging reports update

--- app/models/book_stats.rb
+++ app/models/book_stats.rb
@@ -7,6 +7,7 @@ class BookStats < ActiveRecord::Base
     if !(book_stats)
       book_stats = BookStats.new :book_id => book.id
     end
+    puts "book id is #{book.id}"
     book_stats.total_images = DynamicDescription.connection.select_value("select count(id) from dynamic_images where book_id = '#{book.id}'")
     book_stats.total_essential_images = DynamicImage.connection.select_value("select count(id) from dynamic_images where book_id = '#{book.id}' and should_be_described = true")
     book_stats.total_images_described = DynamicDescription.connection.select_value("select count(distinct(dynamic_image_id)) from dynamic_descriptions where book_id = '#{book.id}'")
@@ -14,7 +15,7 @@ class BookStats < ActiveRecord::Base
     (select dynamic_image_id from dynamic_descriptions where dynamic_descriptions.book_id = '#{book.id}' and is_current = 1) as essential
     WHERE book_id = '#{book.id}' and should_be_described = true and di.id = essential.dynamic_image_id ;")
     book_stats.approved_descriptions = DynamicDescription.connection.select_value("SELECT count(id) from dynamic_descriptions where is_current = 1 and book_id = '#{book.id}'")
-
+    puts "approved descriptions = #{book_stats.approved_descriptions}"
     book_stats.save!
   end
 end
