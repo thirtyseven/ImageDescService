672c688dfb8dda1c9b7894ad30e2429d9f3d388f
Eric Hansen <ehansen486@gmail.com>
2012-02-09 22:19:33 -0800

    fix CSV export

--- app/admin/book_stats.rb
+++ app/admin/book_stats.rb
@@ -4,6 +4,30 @@ ActiveAdmin.register BookStats, :as => "Reports" do
   
   actions :index
   
+  csv do
+    column "Uid" do |book_stats|
+       book_stats.book.uid
+    end  
+    column "Title" do |book_stats| 
+       book_stats.book.title
+    end
+    column :total_images
+    column :total_essential_images
+    column "% Essential" do |book_stats|
+       number_to_percentage(book_stats.percent_essential, :precision => 1)
+    end
+    column :total_images_described
+    column :essential_images_described
+    column "% Essential Described"  do |book_stats|
+      if (book_stats.total_essential_images == 0 || book_stats.total_images_described == 0)
+        number_to_percentage(0, :precision => 1)
+      else
+        number_to_percentage(book_stats.percent_essential_described, :precision => 1)
+     end
+    end
+    column :approved_descriptions
+  end
+  
   index do
     column "Uid" do |book_stats|
        book_stats.book.uid
