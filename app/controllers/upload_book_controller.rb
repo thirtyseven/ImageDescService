f5a7389a02ff3ddafe30a1ba3b13de516de38dc3
Eric Hansen <ehansen486@gmail.com>
2012-04-04 14:00:24 -0700

    don't rely on the session for the daisy_directory

--- app/controllers/upload_book_controller.rb
+++ app/controllers/upload_book_controller.rb
@@ -73,8 +73,8 @@ class UploadBookController < ApplicationController
     end
 
     begin
-      accept_book(book.path)
-      xml = get_xml_from_dir
+      daisy_directory = accept_book(book.path)
+      xml = get_xml_from_dir daisy_directory
       doc = Nokogiri::XML xml
       @book_uid = extract_book_uid(doc)
       doc = nil
@@ -128,8 +128,8 @@ class UploadBookController < ApplicationController
   end
 
 private
-  def get_xml_from_dir
-    book_directory = session[:daisy_directory]
+  def get_xml_from_dir daisy_directory = nil
+    book_directory = daisy_directory || session[:daisy_directory]
     contents_filename = get_daisy_contents_xml_name(book_directory)
     File.read(contents_filename)
   end
