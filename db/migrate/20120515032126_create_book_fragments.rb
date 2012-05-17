c2ccbda1ddd856f2f2ab9aa142438d26e31a727f
Eric Hansen <ehansen486@gmail.com>
2012-05-17 16:07:18 -0700

    update the migration to correctly add book_fragment_id to dynamic_images rather than dynamic_descriptions

--- db/migrate/20120515032126_create_book_fragments.rb
+++ db/migrate/20120515032126_create_book_fragments.rb
@@ -9,10 +9,11 @@ class CreateBookFragments < ActiveRecord::Migration
     
     execute "insert into book_fragments (created_at, updated_at, book_id, sequence_number) select now(), now(), id, 1 from books"
     
-    change_table :dynamic_descriptions do |t|
+    change_table :dynamic_images do |t|
       t.integer :book_fragment_id
     end
-    add_constraint 'dynamic_descriptions', 'dynamic_descriptions_book_frag_id', 'book_fragment_id', 'book_fragments', 'id'
+    add_constraint 'dynamic_images', 'dynamic_images_book_frag_id', 'book_fragment_id', 'book_fragments', 'id'
+    execute "update dynamic_images set book_fragment_id = (select id from book_fragments where book_fragments.book_id = dynamic_images.book_id limit 1)"
     
     # Grab the HTML for each book from S3
     repository = RepositoryChooser.choose
@@ -23,7 +24,7 @@ class CreateBookFragments < ActiveRecord::Migration
       if (html)
         # save the HTML file back
         # write the doc.to_html to a temp file, then pass the temp file path to the store_file method
-        file = Tempfile.new(book.uid)
+        file = Tempfile.new(book.uid, :encoding => html.encoding)
         
         file.write html
         file.flush
@@ -36,8 +37,8 @@ class CreateBookFragments < ActiveRecord::Migration
   end
 
   def self.down
-    add_constraint 'dynamic_descriptions', 'dynamic_descriptions_book_frag_id'
-    change_table :dynamic_descriptions do |t|
+    add_constraint 'dynamic_images', 'dynamic_images_book_frag_id'
+    change_table :dynamic_images do |t|
       t.remove :book_fragment_id
     end
     drop_table :book_fragments
