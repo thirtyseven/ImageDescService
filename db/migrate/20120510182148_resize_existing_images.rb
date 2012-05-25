c3b1081f2a40dbbe554e1c960301fef4d7751e30
Rom Srinivasan <roms@benetech.org>
2012-05-25 14:28:18 -0700

    revert accidental check-in

--- db/migrate/20120510182148_resize_existing_images.rb
+++ db/migrate/20120510182148_resize_existing_images.rb
@@ -2,40 +2,39 @@ class ResizeExistingImages < ActiveRecord::Migration
   include RepositoryChooser
 
   def self.up
-      repository = RepositoryChooser.choose
-                       #Client.where(:orders_count => [1,3,5])
-      #Client.limit(5).offset(30)
-      begin
+    repository = RepositoryChooser.choose
 
-        begin
-          repository = RepositoryChooser.choose
-          DynamicImage.where("book_id > 45 and book_id < 60").each do |image|
-            if (image.book )
+    begin
 
-              book_file_name = image.book.uid + ".html"
-              puts "book file name is #{book_file_name}"
-              html = repository.get_cached_html(image.book.uid, book_file_name) rescue nil
-              if (html)
-                puts "in if statement"
-                FileUtils.makedirs(File.join("", "tmp", image.book.uid, "images"))
-                file_name = ::File.join("", "tmp", image.book.uid, image.image_location)
+      DynamicImage.all.each do |image|
+        if (image.book)
 
-                local_file = repository.read_file(image.book.uid + "/" + image.image_location, file_name)
-                if local_file
-                  actual_file = File.open(local_file, "rb")
-                  puts "#{local_file} found"
-                  image.update_attribute("physical_file", actual_file)
-                  File.delete(local_file)
-                end
+          book_file_name = image.book.uid + ".html"
+          html = repository.get_cached_html(image.book.uid, book_file_name) rescue nil
+          if (html)
 
-              end
+            FileUtils.makedirs(File.join("", "tmp", image.book.uid, "images"))
+            file_name = ::File.join("", "tmp", image.book.uid, image.image_location)
+
+            local_file = repository.read_file(image.book.uid + "/" + image.image_location, file_name)
+            if local_file
+              actual_file = File.open(local_file, "rb")
+              puts "#{local_file} found"
+              image.update_attribute("physical_file", actual_file)
             end
-          end
-          int i = 3
-        end
 
+          end
+        end
       end
+
+      rescue Exception => e
+        puts "Unknown problem resizing existing images"
+        puts "#{e.class}: #{e.message}"
+        puts e.backtrace.join("\n")
+        $stderr.puts e
+
     end
+  end
 
   def self.down
   end
