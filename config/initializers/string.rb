515f4c8a43fb722f3e3a0aaae95cf15e6baad098
Eric Hansen <ehansen486@gmail.com>
2012-06-04 23:29:46 -0700

    try to use iconv to force utf8

---
+++ config/initializers/string.rb
@@ -0,0 +1,10 @@
+require 'iconv'
+
+class String
+  IC_UTF8 = Iconv.new('UTF-8//IGNORE', 'UTF-8')
+  def force_utf8
+    #note added extra space plus chop to fix certain cases.
+    IC_UTF8.iconv("#{self} ")[0..-2]
+  end
+  
+end
