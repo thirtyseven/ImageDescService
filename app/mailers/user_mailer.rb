534d06faee1af27e0c10f6b3a7b6ee1c4d881513
Rom Srinivasan <roms@benetech.org>
2012-06-22 13:04:51 -0700

    minor copy changes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default :from => "support@benentech.com"
+  default :from => "do-not-reply@benetech.org"
   
   def book_uploaded_email(user, book)
    @user=user
    @book=book
-   mail(to: user.email, subject: 'Poet Tool Book Uploaded')
+   mail(to: user.email, subject: 'Poet book upload completed')
   end
 end
