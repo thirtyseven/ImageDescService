1e4ac5df192d1ce497d029c66096ee1ecc268013
Eric Hansen <ehansen486@gmail.com>
2012-06-22 11:25:53 -0700

    rename the mailer files and set the member variables to be used in the content

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,8 @@ class UserMailer < ActionMailer::Base
   default :from => "support@benentech.com"
   
   def book_uploaded_email(user, book)
-   @user
-   @book
+   @user=user
+   @book=book
    mail(to: user.email, subject: 'Poet Tool Book Uploaded')
   end
 end
