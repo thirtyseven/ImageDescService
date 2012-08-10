cb55e651e633457fd42b2ecb2a91784343a7232b
Eric Hansen <ehansen486@gmail.com>
2012-08-09 21:34:47 -0700

    reorder the copyright

--- lib/active_admin_views_pages_base.rb
+++ lib/active_admin_views_pages_base.rb
@@ -5,8 +5,8 @@ class ActiveAdmin::Views::Pages::Base < Arbre::HTML::Document
   # Renders the content for the footer
   def build_footer
     div :id => "footer" do
-      para "Copyright &copy; #{Date.today.year.to_s} #{link_to('Benetech', 'http://www.benetech.org')}.".html_safe
       para "<strong>Legal Disclaimer</strong>: The Poet tool is intended to assist in the creation of accessible content. Use of the Poet tool is limited to activities for creating content that comply with applicable copyright law and contractual agreements."
+      para "Copyright &copy; #{Date.today.year.to_s} #{link_to('Benetech', 'http://www.benetech.org')}.".html_safe
       
     end
   end
