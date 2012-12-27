0313ea9652ede2c95f02bf8be5b53dc7a7a53f22
Rom Srinivasan <roms@benetech.org>
2012-12-27 15:07:40 -0800

    adding logs for delete

--- app/helpers/delete_book_job.rb
+++ app/helpers/delete_book_job.rb
@@ -7,4 +7,16 @@ class DeleteBookJob < Struct.new(:book_id)
     book = Book.find(book_id)
     book.destroy
   end
+
+  def before(job)
+    puts 'before book delete'
+  end
+
+  def after(job)
+    puts 'after book delete'
+  end
+
+  def success(job)
+    puts "delete job successfully completed"
+  end
 end
