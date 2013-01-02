00b23274070a421ae82dfc50e45090d04afcc513
Eric Hansen <ehansen486@gmail.com>
2013-01-02 09:06:49 -0800

    IMG-405 Google analytics for Poet add in a way to track google analytics separately for the two prod instances of poet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,17 @@ module ApplicationHelper
     html.gsub "\n", '\\n'
   end
   
+  def render_ga ga_id
+    raw "<script type=\"text/javascript\">
+  
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', '#{ga_id}']);
+    _gaq.push(['_trackPageview']);
+  
+    (function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();
+  
+    </script>
+    "
+  end
+  
 end
