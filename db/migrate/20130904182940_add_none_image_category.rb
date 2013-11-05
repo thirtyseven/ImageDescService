e8694c63d379d3c09a74d20c07b5ee952ed4d4d0
Martin Quiazon <martinq@benetech.org>
2013-11-05 14:42:40 -0800

    Forced reload of column info to make full migrations happy

--- db/migrate/20130904182940_add_none_image_category.rb
+++ db/migrate/20130904182940_add_none_image_category.rb
@@ -1,6 +1,7 @@
 class AddNoneImageCategory < ActiveRecord::Migration
   def self.up
-     ImageCategory.create :name => "None",  :sample_file_name => "image-categories/blank.html", :order_to_display => 0
+    ImageCategory.reset_column_information
+    ImageCategory.create :name => "None",  :sample_file_name => "image-categories/blank.html", :order_to_display => 0
   end
 
   def self.down
