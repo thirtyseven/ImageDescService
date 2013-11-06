a9070b0842a6a3f36d6981656335fe78e3626353
Eric Hansen <ehansen486@gmail.com>
2013-11-06 00:09:29 -0800

    IMG-367 Enable image search within a Poet library made it so that all descriptions are displayed if there are no search filter terms added a 'none' category in the search page.  Before it was defaulting to the first category which happened to have no results; was not very obvious

--- app/controllers/dynamic_descriptions_controller.rb
+++ app/controllers/dynamic_descriptions_controller.rb
@@ -108,24 +108,22 @@ class DynamicDescriptionsController < ApplicationController
     user_library_id =  current_user.user_libraries.first.library_id
     search_submitter_by = params['search']['submitter_by']
 
-    if search_term.present? || search_title.present? || search_isbn.present? || search_image.present? || search_image_type.present? || search_submitter_by.present?
-      @host =  @repository.get_host(request)
-      @results = DynamicDescription.tire.search(:per_page => 20, :page => (params[:page] || 1)) do  
-       query do
-         boolean do
-           must   { term :body, search_term } if search_term.present?
-           must   { term :title, search_title } if search_title.present?
-           must   { term :isbn, search_isbn } if search_isbn.present?
-           must   { term :dynamic_image_id, search_image } if search_image.present?
-           must   { term :image_type, search_image_type } if search_image_type.present?
-           must   { term :submitter_by, search_submitter_by } if search_submitter_by.present?
-           must   { term :is_last_approved, '1' }
-           must   { term :dynamic_description_library_id, user_library_id}
-         end
+    @host =  @repository.get_host(request)
+    @results = DynamicDescription.tire.search(:per_page => 20, :page => (params[:page] || 1)) do  
+     query do
+       boolean do
+         must   { term :body, search_term } if search_term.present?
+         must   { term :title, search_title } if search_title.present?
+         must   { term :isbn, search_isbn } if search_isbn.present?
+         must   { term :dynamic_image_id, search_image } if search_image.present?
+         must   { term :image_type, search_image_type } if search_image_type.present?
+         must   { term :submitter_by, search_submitter_by } if search_submitter_by.present?
+         must   { term :is_last_approved, '1' }
+         must   { term :dynamic_description_library_id, user_library_id}
        end
-      end
-      @dynamic_description_hash = DynamicDescription.where(:id => @results.map(&:id)).all.inject({}){|acc, desc| acc[desc.id] = desc; acc}
+     end
     end
+    @dynamic_description_hash = DynamicDescription.where(:id => @results.map(&:id)).all.inject({}){|acc, desc| acc[desc.id] = desc; acc}
   end
   
   def body_history
