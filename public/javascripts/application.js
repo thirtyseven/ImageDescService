415444004ac9751393d086602ca7723941fcb88f
Edward Kaplan <ekaplan@mdlogix.com>
2014-06-10 01:38:36 -0700

    Add debounce to window.Poet, apply to equation-editor

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -64,6 +64,40 @@ jQuery(function($) {
     });
 
   window.Poet = {
+    /* Adapted from underscore.js */
+    debounce: function (func, wait, immediate) {
+      var timeout, args, context, timestamp, result;
+
+      var later = function() {
+        var last = Date.now() - timestamp;
+
+        if (last < wait && last > 0) {
+          timeout = setTimeout(later, wait - last);
+        } else {
+          timeout = null;
+          if (!immediate) {
+            console.log('calling');
+            result = func.apply(context, args);
+            if (!timeout) context = args = null;
+          }
+        }
+      };
+      return function() {
+        context = this;
+        args = arguments;
+        timestamp = Date.now();
+        var callNow = immediate && !timeout;
+        if (!timeout) {
+          timeout = setTimeout(later, wait);
+        }
+        if (callNow) {
+          result = func.apply(context, args);
+          context = args = null;
+        }
+
+        return result;
+      };
+    },
     imageCategoryPostSave: function(imageId, categoryId) {
       return function(data, status) {
         for (var i = 0; i < window.top.frames.length; i++) {
